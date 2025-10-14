92ab644f49f08b14cc572edbf5c8ffd49af6c7ad
Hasrat Hussain <118108611+hasratmd697@users.noreply.github.com>
2025-10-14 15:57:04 +0000

    created main.py and added post endpoint

---
+++ project_1/student/main.py
@@ -0,0 +1,21 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "fastapi[standard]",
+#   "uvicorn",
+# ]
+# ///
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# post endpoint that receives a json object with following fields: email, secret, task, round, nonce,
+# briefs, checks(array), evaluation_url, attachments(array with bobjects with url and name fields)
+
+@app.post("/handle_task")
+async def handle_task(data: dict):
+    # just return the received data for now
+    print(data)
+    return {"message": "Task_received", "data": data}
